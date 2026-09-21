0879c5ca4b1ddb46738b43a5af57cc5b58aec6c6
Personal <personal@MSI-LEVY.168.1.17>
2026-09-21 12:52:21 -0300

    Fix: accounts

--- app/Http/Controllers/Auth/AuthenticatedSessionController.php
+++ app/Http/Controllers/Auth/AuthenticatedSessionController.php
@@ -21,13 +21,10 @@ class AuthenticatedSessionController extends Controller
             'canResetPassword' => Route::has('password.request'),
             'status' => $request->session()->get('status'),
             'demoAccounts' => app()->environment('local') ? [
-                ['role' => 'Paciente', 'name' => 'Juan Paciente', 'email' => 'juan@sanbenito.test'],
-                ['role' => 'Paciente', 'name' => 'Laura Paciente', 'email' => 'laura@sanbenito.test'],
-                ['role' => 'Doctor', 'name' => 'Ana Pérez', 'email' => 'ana.perez@sanbenito.test'],
-                ['role' => 'Doctor', 'name' => 'Luis Gómez', 'email' => 'luis.gomez@sanbenito.test'],
-                ['role' => 'Doctor', 'name' => 'María López', 'email' => 'maria.lopez@sanbenito.test'],
-                ['role' => 'Admin', 'name' => 'Admin San Benito', 'email' => 'admin@sanbenito.test'],
-                ['role' => 'Super admin', 'name' => 'Super Admin', 'email' => 'superadmin@sanbenito.test'],
+                ['role' => 'Super admin', 'name' => 'Super admin', 'email' => 'superadmin@test.test'],
+                ['role' => 'Admin', 'name' => 'Secretaria', 'email' => 'admin@test.test'],
+                ['role' => 'Doctor', 'name' => 'Doctor', 'email' => 'doctor@test.test'],
+                ['role' => 'Paciente', 'name' => 'Paciente', 'email' => 'paciente@test.test'],
             ] : [],
         ]);
     }
